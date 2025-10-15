b538f594cfcea18de1b75608de432f69d1808f4a
somy10 <guptasomya798@gmail.com>
2025-10-15 23:49:26 +0530

    push properties files

---
+++ src/test/java/cabBooking/example/cabBooking/CabBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package cabBooking.example.cabBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

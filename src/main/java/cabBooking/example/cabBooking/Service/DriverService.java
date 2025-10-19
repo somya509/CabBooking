5192e599c78b29eb4bac1daac1aa3363611dfdf8
somy10 <guptasomya798@gmail.com>
2025-10-19 22:28:00 +0530

    changes for jira #SCRUM-6 :added new  classes & first driver available

--- src/main/java/cabBooking/example/cabBooking/Service/DriverService.java
+++ src/main/java/cabBooking/example/cabBooking/Service/DriverService.java
@@ -1,9 +1,6 @@
 package cabBooking.example.cabBooking.Service;
 
-import cabBooking.example.cabBooking.dto.Driver;
-import cabBooking.example.cabBooking.dto.MatchingStrategy;
-import cabBooking.example.cabBooking.dto.NearestAvailableStrategy;
-import cabBooking.example.cabBooking.dto.location;
+import cabBooking.example.cabBooking.dto.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,9 +13,14 @@ public class DriverService {
         List<Driver> drivers1= Arrays.asList(new Driver(49,799,false,new location(64,86),76,"sgh",986));
         List<Driver> drivers3= Arrays.asList(new Driver(48,749,true,new location(60,876),776,"sghugjh",346));
 
-location loc =new location(5,8);
+        location loc =new location(5,8);
         MatchingStrategy matchingStrategy=new NearestAvailableStrategy();
-        Optional<Driver> nearest = matchingStrategy.findDriver(loc, drivers);
+        MatchingStrategy matchingStrategy1=new FirstAvailableStrategy();
+        Optional<Driver> nearest = matchingStrategy.findDriver(loc, drivers3);
         nearest.ifPresentOrElse(driver -> System.out.println("nearest available driver :"+ driver.getName() + "at :" + driver.getLocation()),()-> System.out.println("❌ No available drivers found."));
+      
+        Optional<Driver> findFirst =matchingStrategy1.findDriver(loc,drivers);
+        findFirst.ifPresentOrElse(driver -> System.out.println("first driver available :" + driver.getName() + "at :" + driver.getLocation()),()-> System.out.println("❌ No available drivers found ."));
     }
+
 }
